0cda3ab9df61292caf2fac8d0c8837daaf38faaa
Prafulmhrjn <praful.mhrjn29@gmail.com>
2021-07-12 17:50:45 +0545

    calculator

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,26 @@
 from tkinter import *
 
 root = Tk()
+root.geometry("420x430")
+
 
 #Giving the title of the project
 root.title("Simple Calculator")
 
-e=Entry(root, width=50, borderwidth=10)
+e=Entry(root, width=50, borderwidth=20,fg="blue")
 e.grid(row=0,column=0,columnspan=6,padx=10,pady=10)
+root.configure(background="orange")
 
 #click function
 def button_click(number):
     current = e.get()
     e.delete(0,END)
     e.insert(0, str(current) + str(number))
+
+def button_erase(number):
+    current = e.get()
+
+    e.delete(0,str(current) - str(number))
 
 
 def button_clear():
@@ -36,6 +44,7 @@ def button_equals():
 
     if math == "percent":
         e.insert(0,f_number/100)
+
 
 
 
@@ -87,23 +96,24 @@ def button_percentage():
 
 
 
-button_1=Button(root, text="1",padx=40,pady=20,command=lambda:button_click(1))
-button_2=Button(root, text="2",padx=40,pady=20,command=lambda:button_click(2))
-button_3=Button(root, text="3",padx=40,pady=20,command=lambda:button_click(3))
-button_4=Button(root, text="4",padx=40,pady=20,command=lambda:button_click(4))
-button_5=Button(root, text="5",padx=40,pady=20,command=lambda:button_click(5))
-button_6=Button(root, text="6",padx=40,pady=20,command=lambda:button_click(6))
-button_7=Button(root, text="7",padx=40,pady=20,command=lambda:button_click(7))
-button_8=Button(root, text="8",padx=40,pady=20,command=lambda:button_click(8))
-button_9=Button(root, text="9",padx=40,pady=20,command=lambda:button_click(9))
-button_0=Button(root, text="0",padx=40,pady=20,command=lambda:button_click(0))
-button_percentage=Button(root, text="%",padx=40,pady=20,command=button_percentage)
-button_equals=Button(root, text="=",padx=90,pady=20,command=button_equals)
-button_clear=Button(root, text="AC",padx=85,pady=20,command=button_clear)
-button_add=Button(root, text="+",padx=40,pady=20,command=button_add)
-button_subtract=Button(root, text="-",padx=40,pady=20,command=button_subtract)
-button_multiply=Button(root, text="x",padx=40,pady=20,command=button_multiply)
-button_divide=Button(root, text="/",padx=40,pady=20,command=button_divide)
+button_1=Button(root, text="1",padx=40,pady=20,borderwidth=5,command=lambda:button_click(1))
+button_2=Button(root, text="2",padx=40,pady=20,borderwidth=5,command=lambda:button_click(2))
+button_3=Button(root, text="3",padx=40,pady=20,borderwidth=5,command=lambda:button_click(3))
+button_4=Button(root, text="4",padx=40,pady=20,borderwidth=5,command=lambda:button_click(4))
+button_5=Button(root, text="5",padx=40,pady=20,borderwidth=5,command=lambda:button_click(5))
+button_6=Button(root, text="6",padx=40,pady=20,borderwidth=5,command=lambda:button_click(6))
+button_7=Button(root, text="7",padx=40,pady=20,borderwidth=5,command=lambda:button_click(7))
+button_8=Button(root, text="8",padx=40,pady=20,borderwidth=5,command=lambda:button_click(8))
+button_9=Button(root, text="9",padx=40,pady=20,borderwidth=5,command=lambda:button_click(9))
+button_0=Button(root, text="0",padx=40,pady=20,borderwidth=5,command=lambda:button_click(0))
+button_percentage=Button(root, text="%",padx=40,pady=20,borderwidth=5,command=button_percentage)
+button_equals=Button(root, text="=",padx=90,pady=20,borderwidth=5,command=button_equals)
+button_clear=Button(root, text="AC",padx=85,pady=20,borderwidth=5,command=button_clear)
+button_add=Button(root, text="+",padx=40,pady=20,borderwidth=5,command=button_add)
+button_subtract=Button(root, text="-",padx=40,pady=20,borderwidth=5,command=button_subtract)
+button_multiply=Button(root, text="x",padx=40,pady=20,borderwidth=5,command=button_multiply)
+button_divide=Button(root, text="/",padx=40,pady=20,borderwidth=5,command=button_divide)
+button_erase=Button(root, text="clear",padx=30,pady=20,borderwidth=5,command=button_erase)
 
 
 
@@ -125,6 +135,17 @@ button_add.grid(row=2,column=4)
 button_subtract.grid(row=3,column=4)
 button_multiply.grid(row=4,column=4)
 button_divide.grid(row=5,column=4)
+button_erase.grid(row=6,column=4)
+
+
+root.iconbitmap('C:/Users/Prafulmhrjn/OneDrive/Desktop/calculator.ico')
+
+
+
+
+
+
+
 
 
 
